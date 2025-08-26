99b891612b831fa17a86b5510fdef9822f24635f
Britt Franken <britt.franken9@gmail.com>
2025-08-26 21:09:59 +0200

    JAAAA

--- src/config/passport.js
+++ src/config/passport.js
@@ -17,8 +17,8 @@ passport.deserializeUser(async (id, done) => {
 });
 
 // src/config/passport.js
-const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_CALLBACK_URL } = process.env;
-if (!GOOGLE_CLIENT_ID || !GOOGLE_CLIENT_SECRET || !GOOGLE_CALLBACK_URL) {
+const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = process.env;
+if (!GOOGLE_CLIENT_ID || !GOOGLE_CLIENT_SECRET) {
   throw new Error('Google OAuth env vars missing. Set GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_CALLBACK_URL.');
 }
 
